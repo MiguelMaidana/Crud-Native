eaecd2e49f2d337b1aeffe5204b2e2a6f7a0c0e0
Miguel Maidana <maidana.miguell@gmail.com>
2020-05-06 20:23:08 -0300

    Creo el componenete Nuevo Cliente con todos sus Campos (estilo con  Paper)

--- view/NuevoCliente.js
+++ view/NuevoCliente.js
@@ -1,14 +1,50 @@
 import React from "react"
-import {Text , StyleSheet} from "react-native"
+import {View , StyleSheet} from "react-native"
+import {TextInput, Headline,Button} from "react-native-paper"
+import globalStyles from "../styles/global"
 
 const NuevoCLiente =()=>{
+
+    const leerNombre=()=>{
+        console.log("escribiendo......")
+    }
     return (
-        <Text>Desde Nuevo Cliente</Text>
+        <View style={globalStyles.contenedor}>
+            <Headline style={globalStyles.titulo}>Añadir Nuevo Cliente</Headline>
+            <TextInput
+                label="Nombre"
+                placeholder="Miguel"
+                onChangeText={()=>leerNombre()}
+                style={styles.input}
+            />
+              <TextInput
+                label="Telefono"
+                placeholder="EJ : 1134434324"
+                onChangeText={()=>leerNombre()}
+                style={styles.input}
+            />
+              <TextInput
+                label="Correo"
+                placeholder="correo@correo"
+                onChangeText={()=>leerNombre()}
+                style={styles.input}
+            />
+              <TextInput
+                label="Empresa"
+                placeholder="Nombre Empresa"
+                onChangeText={()=>leerNombre()}
+                style={styles.input}
+            />
+           
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    input :{
+        marginBottom:20,
+        backgroundColor:"transparent"
+    }
 })
 
 
