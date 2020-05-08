7610a1d54b0eb24f295699c997cd20e570a77c01
Miguel Maidana <maidana.miguell@gmail.com>
2020-05-08 08:34:14 -0300

    Obtengo los clientes y los muestros en App.js. Armo un header condicional en caso de que tenga clientes la API o no

--- view/Inicio.js
+++ view/Inicio.js
@@ -1,9 +1,41 @@
-import React from "react"
-import {Text , StyleSheet} from "react-native"
+import React,{useState,useEffect} from "react"
+import {Text , StyleSheet, FlatList, View} from "react-native"
+import axios from "axios"
+import {List, Headline} from "react-native-paper"
+import globalStyles from "../styles/global"
 
 const Inicio =()=>{
+
+    const [clientes,guardarClientes] = useState([])
+
+    useEffect(()=>{
+
+        const obtnerClientesApi = async () =>{
+            try{
+                const resultado = await axios.get("http://192.168.0.10:19001/clientes")
+                guardarClientes(resultado.data)
+            }catch(error){
+                console.log(error)
+            }
+        }
+        obtnerClientesApi()
+
+    },[])
     return (
-        <Text>Desde Inicio</Text>
+        <View style={globalStyles.contenedor}>
+            <Headline style={globalStyles.titulo}>{clientes.length > 0 ? "Clientes" : " Aun no hay clientes"}</Headline>
+
+            <FlatList
+                data={clientes}
+                keyExtractor={cliente => (cliente.id)}
+                renderItem={({item})=>(
+                    <List.Item
+                        title={item.nombre}
+                        description={item.empresa}
+                    />
+                )}
+            />
+        </View>
     )
 }
 
